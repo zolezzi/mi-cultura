66eec082a0b6b306e59b67bfc92bb895a7389dd9
zolezzi <cezcardozo23@gmail.com>
2023-08-17 19:45:44 -0300

    Adding project skeleton

---
+++ src/main/java/unq/edu/li/pdes/micultura/MiCulturaApplication.java
@@ -0,0 +1,13 @@
+package unq.edu.li.pdes.micultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCulturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCulturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unq/edu/li/pdes/micultura/MiCulturaApplicationTests.java
@@ -0,0 +1,13 @@
+package unq.edu.li.pdes.micultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCulturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
