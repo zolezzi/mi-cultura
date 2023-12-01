12ff6da965e321982372ae452896cd4e972def32
zolezzi <cezcardozo23@gmail.com>
2023-11-30 23:24:29 -0300

    [Backend] Added integration test login and create new user with account

---
+++ src/test/java/unq/edu/li/pdes/micultura/configuration/TestSecurityConfig.java
@@ -0,0 +1,17 @@
+package unq.edu.li.pdes.micultura.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@TestConfiguration
+@EnableWebSecurity
+public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests().anyRequest().permitAll();
+    }
+}
