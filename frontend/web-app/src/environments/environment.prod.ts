e62fa8eb849b7dcf12932673f4644c68657dde0e
zolezzi <cezcardozo23@gmail.com>
2023-12-12 02:43:32 -0300

    Added path api and change path folder static files

--- frontend/web-app/src/environments/environment.prod.ts
+++ frontend/web-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://mi-cultura-production.up.railway.app',
+    apiUrl: 'https://mi-cultura-production.up.railway.app/api',
   };
